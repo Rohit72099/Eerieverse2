db143faccbce731d1dc35223d120767e383eb24f
Rohit72099 <rk464152@gmail.com>
2025-04-01 14:29:54 +0530

    route fixed

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ let mongoose = require('mongoose');
 const router = require('./app/routes/user.route');
 const path = require('path');
 
-
 let cors = require('cors');
 let cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 let app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", 
+    "default-src 'self'; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "font-src 'self' https://fonts.gstatic.com https://eerieverse2.vercel.app; " +
+    "img-src 'self' data:; " +
+    "connect-src 'self' https://eerieverse2.vercel.app; " +
+    "object-src 'none';"
+  );
+  next();
+});
 
 
 app.use(cookieParser());
@@ -23,24 +35,12 @@ app.set('views', path.join(__dirname, 'app', 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", 
-      "default-src 'self'; " +
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; " +
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-      "font-src 'self' https://fonts.gstatic.com https://eerieverse2.vercel.app; " +
-      "img-src 'self' data:; " +
-      "connect-src 'self' https://eerieverse2.vercel.app; " +
-      "object-src 'none';"
-    );
-    next();
-  });
+
   
 
 
-
 //import routes
-app.use('/api/user',router);
+app.use(router);
 
 
 
