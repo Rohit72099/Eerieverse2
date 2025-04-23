316b51c6c50c95f1e794167f8e31774323d86dde
Rohit72099 <rk464152@gmail.com>
2025-04-24 01:44:48 +0530

    jenkinsfixed1

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,10 +1,28 @@
+# #!/usr/bin/env sh
+
+# npm start
+
+# npm start &
+# sleep 1
+# echo $! > .pidfile
+
+# echo 'Now...'
+# echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+
+
+
 #!/usr/bin/env sh
 
-npm run build
-
+# Start app in background
 npm start &
 sleep 1
+
+# Save PID
 echo $! > .pidfile
 
-echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+# Read port from .env or use 8000 as fallback
+PORT=${PORT:-$(grep PORT .env | cut -d '=' -f2)}
+PORT=${PORT:-8000}
+
+echo "Now..."
+echo "Visit http://localhost:$PORT to see your Node.js application in action."
