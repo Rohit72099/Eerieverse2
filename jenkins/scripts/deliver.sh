ffde7bb193f988ff62d0c8a90367aac1a4ba6e86
Rohit72099 <rk464152@gmail.com>
2025-04-24 23:27:10 +0530

    fixed3

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-npm start
+# npm run build
 
-npm start &
-sleep 1
-echo $! > .pidfile
+# npm start &
+# sleep 1
+# echo $! > .pidfile
 
-echo 'Now...'
-echo 'Visit http://localhost:8000 to see your Node.js/React application in action.'
+# echo 'Now...'
+# echo 'Visit http://localhost:8000 to see your Node.js/React application in action.'
 
 
 
